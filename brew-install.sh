5e0200f46d997d79097681f20f78dcda182ef2fd
alstamber <alstamber@gmail.com>
2014-05-05 20:57:44 +0900

    Fix script

--- brew-install.sh
+++ brew-install.sh
@@ -4,7 +4,10 @@ if [ ! -x "$(which brew)" ]; then
     ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     export HOMEBREW_CASK_OPTS="--appdir=/Applications"
     brew bundle
-    cp -f /usr/local/Cellar/ricty/3.2.2/share/fonts/Ricty*.ttf ~/Library/Fonts/
+    cp -f /usr/local/Cellar/ricty/3.2.2/share/fonts/Ricty\ Regular\ for\ Powerline.ttf ~/Library/Fonts
+    cp -f /usr/local/Cellar/ricty/3.2.2/share/fonts/Ricty\ Bold\ for\ Powerline.ttf ~/Library/Fonts
+    cp -f /usr/local/Cellar/ricty/3.2.2/share/fonts/Ricty-Regular.ttf ~/Library/Fonts
+    cp -f /usr/local/Cellar/ricty/3.2.2/share/fonts/Ricty-Bold.ttf ~/Library/Fonts
     fc-cache -vf
 else
     echo "Homebrew is already installed."
