faf6a47cf2a92baa661b741dbbd4ff1881f43037
Ryutaro Yamamoto <r.yamamoto@gigei.jp>
2015-08-10 13:55:21 +0900

    shell scriptもインストールできるようにした

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@ do
     if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
     then
         ln -Fis "$PWD/$dotfile" $HOME
-    fi 
+    fi
+done
+
+mkdir -p $HOME/bin 2>/dev/null
+for binfile in bin/*
+do
+    ln -Fis "$PWD/$binfile" $HOME/bin/
 done
